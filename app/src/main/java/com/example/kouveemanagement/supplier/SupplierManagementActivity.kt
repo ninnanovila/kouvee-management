fe7564e71dfea27e179ad5e9895dfb3f2d384777
Ninna Novila <ninnanovila@Ninnas-MacBook-Pro.local>
2020-02-23 23:59:36 +0700

    change supplier [ninna]

--- app/src/main/java/com/example/kouveemanagement/supplier/SupplierManagementActivity.kt
+++ app/src/main/java/com/example/kouveemanagement/supplier/SupplierManagementActivity.kt
@@ -1,6 +1,10 @@
 package com.example.kouveemanagement.supplier
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -14,6 +18,7 @@ import com.example.kouveemanagement.presenter.SupplierPresenter
 import com.example.kouveemanagement.presenter.SupplierView
 import com.example.kouveemanagement.repository.Repository
 import kotlinx.android.synthetic.main.activity_supplier_management.*
+import org.jetbrains.anko.startActivity
 
 
 class SupplierManagementActivity : AppCompatActivity(), SupplierView {
@@ -34,7 +39,6 @@ class SupplierManagementActivity : AppCompatActivity(), SupplierView {
             transaction.replace(R.id.container, fragment).commit()
         }
 
-//        setTabLayout()
     }
 
     override fun showLoading() {
@@ -59,7 +63,7 @@ class SupplierManagementActivity : AppCompatActivity(), SupplierView {
             recyclerview.layoutManager = LinearLayoutManager(this)
             recyclerview.adapter = this.let {
                 SupplierRcyclerViewAdapter(suppliers) {
-                    //                    showDialog(it)
+                    showDialog(it)
                     Toast.makeText(this, it.id, Toast.LENGTH_LONG).show()
                 }
             }
@@ -71,29 +75,28 @@ class SupplierManagementActivity : AppCompatActivity(), SupplierView {
     override fun supplierFailed() {
     }
 
-//    fun setTabLayout(){
-//        viewpager.adapter = object : FragmentStateAdapter(this) {
-//            override fun getItemCount(): Int {
-//                return 2
-//            }
-//
-//            override fun createFragment(position: Int): Fragment {
-//                return when(position){
-//                    0 -> {
-//                        AllSupplierFragment.newInstance()
-//                    }
-//                    else -> {
-//                        AddSupplierFragment.newInstance()
-//                    }
-//                }
-//            }
-//        }
-//
-//        TabLayoutMediator(tablayout, viewpager) {tab, position ->
-//            tab.text = when(position) {
-//                0 -> "Show All"
-//                else -> "Add"
-//            }
-//        }.attach()
-//    }
+    private fun showDialog(supplier: Supplier){
+
+        val dialog = LayoutInflater.from(this).inflate(R.layout.dialog_detail_customer, null)
+
+        val name = dialog.findViewById<TextView>(R.id.name)
+        val address = dialog.findViewById<TextView>(R.id.address)
+        val phone_number = dialog.findViewById<TextView>(R.id.phone_number)
+        val btn_edit = dialog.findViewById<Button>(R.id.btn_edit)
+
+        name.text = supplier.name.toString()
+        address.text = supplier.address.toString()
+        phone_number.text = supplier.phone_number.toString()
+
+        AlertDialog.Builder(this)
+            .setView(dialog)
+            .setTitle("Supplier Info")
+            .show()
+
+        btn_edit.setOnClickListener {
+            startActivity<EditSupplierActivity>("supplier" to supplier)
+        }
+    }
+
+
 }
